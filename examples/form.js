943f9008556b5f0ec6a686b4219dfebd46ae53b7
RaoHai <surgesoft@gmail.com>
2016-11-09 19:27:09 +0800

    rename example

--- examples/form.js
+++ examples/form.js
@@ -1,7 +1,7 @@
 // use jsx to render html, do not modify simple.html
 
-import 'rc-CropViewer/assets/index.less';
-import CropViewer from 'rc-CropViewer';
+import 'rc-cropping/assets/index.less';
+import CropViewer from 'rc-cropping';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Form, Icon, Input, Button, Checkbox } from 'antd';
