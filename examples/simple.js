d89ee2b7603489aff74a0d48fe3c3d7896f12eaf
RaoHai <surgesoft@gmail.com>
2016-11-09 19:23:04 +0800

    Updates

---
+++ examples/simple.js
@@ -0,0 +1,39 @@
+webpackJsonp([1],{
+
+/***/ 0:
+/***/ function(module, exports, __webpack_require__) {
+
+	module.exports = __webpack_require__(781);
+
+
+/***/ },
+
+/***/ 781:
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+	
+	__webpack_require__(!(function webpackMissingModule() { var e = new Error("Cannot find module \"rc-CropViewer/assets/index.less\""); e.code = 'MODULE_NOT_FOUND'; throw e; }()));
+	
+	var _rcCropViewer = __webpack_require__(!(function webpackMissingModule() { var e = new Error("Cannot find module \"rc-CropViewer\""); e.code = 'MODULE_NOT_FOUND'; throw e; }()));
+	
+	var _rcCropViewer2 = _interopRequireDefault(_rcCropViewer);
+	
+	var _react = __webpack_require__(2);
+	
+	var _react2 = _interopRequireDefault(_react);
+	
+	var _reactDom = __webpack_require__(35);
+	
+	var _reactDom2 = _interopRequireDefault(_reactDom);
+	
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+	
+	// use jsx to render html, do not modify simple.html
+	
+	_reactDom2.default.render(_react2.default.createElement(_rcCropViewer2.default, { circle: true }), document.getElementById('__react-content'));
+
+/***/ }
+
+});
+//# sourceMappingURL=simple.js.map
