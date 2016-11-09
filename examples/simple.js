e610ef0457e53c857590443f5a844167b110fb22
RaoHai <surgesoft@gmail.com>
2016-11-09 19:27:41 +0800

    Updates

--- examples/simple.js
+++ examples/simple.js
@@ -3,27 +3,27 @@ webpackJsonp([1],{
 /***/ 0:
 /***/ function(module, exports, __webpack_require__) {
 
-	module.exports = __webpack_require__(781);
+	module.exports = __webpack_require__(790);
 
 
 /***/ },
 
-/***/ 781:
+/***/ 790:
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
 	
-	__webpack_require__(!(function webpackMissingModule() { var e = new Error("Cannot find module \"rc-CropViewer/assets/index.less\""); e.code = 'MODULE_NOT_FOUND'; throw e; }()));
+	__webpack_require__(2);
 	
-	var _rcCropViewer = __webpack_require__(!(function webpackMissingModule() { var e = new Error("Cannot find module \"rc-CropViewer\""); e.code = 'MODULE_NOT_FOUND'; throw e; }()));
+	var _rcCropping = __webpack_require__(3);
 	
-	var _rcCropViewer2 = _interopRequireDefault(_rcCropViewer);
+	var _rcCropping2 = _interopRequireDefault(_rcCropping);
 	
-	var _react = __webpack_require__(2);
+	var _react = __webpack_require__(6);
 	
 	var _react2 = _interopRequireDefault(_react);
 	
-	var _reactDom = __webpack_require__(35);
+	var _reactDom = __webpack_require__(128);
 	
 	var _reactDom2 = _interopRequireDefault(_reactDom);
 	
@@ -31,7 +31,7 @@ webpackJsonp([1],{
 	
 	// use jsx to render html, do not modify simple.html
 	
-	_reactDom2.default.render(_react2.default.createElement(_rcCropViewer2.default, { circle: true }), document.getElementById('__react-content'));
+	_reactDom2.default.render(_react2.default.createElement(_rcCropping2.default, { circle: true }), document.getElementById('__react-content'));
 
 /***/ }
 
