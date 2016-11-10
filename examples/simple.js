24598135ae9b73d4ffe8774811b85094977c69d4
RaoHai <surgesoft@gmail.com>
2016-11-10 14:29:34 +0800

    Updates

--- examples/simple.js
+++ examples/simple.js
@@ -3,27 +3,33 @@ webpackJsonp([1],{
 /***/ 0:
 /***/ function(module, exports, __webpack_require__) {
 
-	module.exports = __webpack_require__(790);
+	module.exports = __webpack_require__(792);
 
 
 /***/ },
 
-/***/ 790:
+/***/ 792:
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
 	
-	__webpack_require__(2);
+	__webpack_require__(3);
 	
-	var _rcCropping = __webpack_require__(3);
+	var _rcCropping = __webpack_require__(4);
 	
 	var _rcCropping2 = _interopRequireDefault(_rcCropping);
 	
-	var _react = __webpack_require__(6);
+	var _rcDialog = __webpack_require__(537);
+	
+	var _rcDialog2 = _interopRequireDefault(_rcDialog);
+	
+	__webpack_require__(793);
+	
+	var _react = __webpack_require__(7);
 	
 	var _react2 = _interopRequireDefault(_react);
 	
-	var _reactDom = __webpack_require__(128);
+	var _reactDom = __webpack_require__(143);
 	
 	var _reactDom2 = _interopRequireDefault(_reactDom);
 	
@@ -31,9 +37,24 @@ webpackJsonp([1],{
 	
 	// use jsx to render html, do not modify simple.html
 	
-	_reactDom2.default.render(_react2.default.createElement(_rcCropping2.default, { circle: true }), document.getElementById('__react-content'));
+	_reactDom2.default.render(_react2.default.createElement(_rcCropping2.default, {
+	  getSpinContent: function getSpinContent() {
+	    return _react2.default.createElement(
+	      'span',
+	      null,
+	      'loading...'
+	    );
+	  },
+	  renderModal: function renderModal() {
+	    return _react2.default.createElement(_rcDialog2.default, null);
+	  },
+	  circle: true
+	}), document.getElementById('__react-content'));
 
-/***/ }
+/***/ },
+
+/***/ 793:
+3
 
 });
 //# sourceMappingURL=simple.js.map
